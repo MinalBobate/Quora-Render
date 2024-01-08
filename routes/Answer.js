24a22151d0ebeff56568ad0effcef50bb0b82da4
Minal Bobate <minalbobate9112@gmail.com>
2024-01-08 13:31:17 +0530

    answer component updated

--- routes/Answer.js
+++ routes/Answer.js
@@ -3,61 +3,61 @@ const router = express.Router();
 
 import answerDB from "../models/Answer.js";
 
-// router.post("/", async (req, res) => {
-//   try {
-//     await answerDB
-
-//     .create({
-//         answer: req.body.answer,
-//         questionId: req.body.questionId,
-//         user: req.body.user,
-//       })
-//       .then(() => {
-//         res.status(201).send({
-//           status: true,
-//           message: "Answer added successfully",
-
-//         });
-//       })
-//       .catch((e) => {
-//         res.status(400).send({
-//           status: false,
-//           message: "Bad request",
-//         });
-//       });
-//   } catch (e) {
-//     res.status(500).send({
-//       status: false,
-//       message: "Error while adding answer",
-//     });
-//   }
-// });
-
 router.post("/", async (req, res) => {
   try {
-    const answer = await answerDB.create({
-      answer: req.body.answer,
-      question: req.body.questionId,
-      user: req.body.user,
-    });
+    await answerDB
 
-    
-    await Question.findByIdAndUpdate(req.body.questionId, {
-      $push: { answers: answer._id },
-    });
+    .create({
+        answer: req.body.answer,
+        questionId: req.body.questionId,
+        user: req.body.user,
+      })
+      .then(() => {
+        res.status(201).send({
+          status: true,
+          message: "Answer added successfully",
 
-    res.status(201).send({
-      status: true,
-      message: "Answer added successfully",
-    });
+        });
+      })
+      .catch((e) => {
+        res.status(400).send({
+          status: false,
+          message: "Bad request",
+        });
+      });
   } catch (e) {
-    console.error(e);
     res.status(500).send({
       status: false,
       message: "Error while adding answer",
     });
   }
 });
+
+// router.post("/", async (req, res) => {
+//   try {
+//     const answer = await answerDB.create({
+//       answer: req.body.answer,
+//       question: req.body.questionId,
+//       user: req.body.user,
+//     });
+
+    
+//     // await Question.findByIdAndUpdate(req.body.questionId, {
+//     //   $push: { answers: answer._id },
+//     // });
+
+//     res.status(201).send({
+//       status: true,
+//       message: "Answer added successfully",
+//     });
+//   } catch (e) {
+//     console.error(e);
+//     res.status(500).send({
+//       status: false,
+//       message: "Error while adding answer",
+//     });
+//   }
+// });
 
 
 router.get("/:id", async (req, res) => {
